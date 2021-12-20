de77451a0e7d4e936355b2ee905186adf552b4f8
victordurco <victordurco@gmail.com>
2021-12-19 22:48:54 -0300

    server fix

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import "./setup";
 import app, { init } from "./app";
 
 init().then(() => {
-  app.listen(4000, () => {
+  app.listen(process.env.PORT || 4000, () => {
     console.log('Server is listening on port 4000');
   });
 });
